f389592af814f46bdf167a0a26a1fec7a711532c
Daniel Zinger <daniel.zinger@DanielZ-mac.local>
2016-08-19 18:29:07 +0300

    changed installer to just add all the vim files because fuck it

--- install.sh
+++ install.sh
@@ -2,15 +2,7 @@ cd ~/.vim_runtime2
 
 echo 'set runtimepath+=~/.vim_runtime2
 
-source ~/.vim_runtime2/vimrcs/basic.vim
-source ~/.vim_runtime2/vimrcs/filetypes.vim
-source ~/.vim_runtime2/vimrcs/plugins_config.vim
-source ~/.vim_runtime2/vimrcs/extended.vim
-source ~/.vim_runtime2/vimrcs/neovim.vim
-
-try
-source ~/.vim_runtime2/vimrcs/my_configs.vim
-catch
-endtry' > ~/.vimrc
+# currently loading all configurations
+source ~/.vim_runtime2/vimrcs/
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
